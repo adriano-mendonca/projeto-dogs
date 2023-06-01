f0081fdd7140ebca72ef081f5634e3c54c3e3249
Adriano Brito de Mendonça <adrianoskiazy51@gmail.com>
2023-06-01 17:24:10 -0300

    Estilizando a página de login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginCreate from "./LoginCreate";
 import LoginPasswordReset from "./LoginPasswordReset";
+import { UserContext } from "../../UserContext";
 
 const Login = () => {
+    const { login } = React.useContext(UserContext)
+
+    if(login === true) return <Navigate to="/conta" />
+
     return (
         <div>
             <Routes>
