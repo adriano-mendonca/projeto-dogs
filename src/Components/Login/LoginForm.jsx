707552c902e2c61cfd8cd267d2250ac99f65118e
Adriano Brito de Mendonça <adrianoskiazy51@gmail.com>
2023-06-01 11:54:20 -0300

    Novo hook useForm
    Criação do hook useForm e melhorias no fetch

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,26 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../Form/Input";
 import Button from "../Form/Button";
+import useForm from "../../Hooks/useForm";
+import { TOKEN_POST, USER_GET } from "../../api";
 
 const LoginForm = () => {
-    const [username, setUsername] = React.useState('')
-    const [password, setPassword] = React.useState('')
+    const username = useForm()
+    const password = useForm()
 
-    function handleSubmit(event) {
+    React.useEffect(() => {
+        const token = window.localStorage.getItem('token')
+        if(token) getUser(token)
+    }, [])
+
+    async function getUser(token) {
+        const {url, option} = USER_GET(token)
+        const response = await fetch(url, option)
+        const json = await response.json()
+        console.log(json)
+    }
+
+
+    async function handleSubmit(event) {
         event.preventDefault();
 
-        fetch('https://www.dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username, password}),
-        }).then(response => {
-            console.log(response)
-            return response.json()
-        }).then((json) => {
-            console.log(json)
-        })
+        if(username.validate() && password.validate()){
+
+            const {url, options} = TOKEN_POST({
+                username: username.value, 
+                password: password.value
+            })
+
+            const response = await fetch(url, options)
+            const json = await response.json()
+            window.localStorage.setItem('token', json.token)
+
+            getUser(json.token)
+        }
     }
 
     return (
@@ -29,8 +45,8 @@ const LoginForm = () => {
         <h1>Login</h1>
         <form action="" onSubmit={handleSubmit}>
 
-            <Input label="Usuário" type="text" name="username" />
-            <Input label="Senha" type="password" name="password" />
+            <Input label="Usuário" type="text" name="username" {...username} />
+            <Input label="Senha" type="password" name="password" {...password} />
             
 
             <Button>Entrar</Button>
