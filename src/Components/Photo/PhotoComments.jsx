5af93574cd0aee33bfff0a2fd75e94461ce13b0b
Adriano Brito de Mendonça <adrianoskiazy51@gmail.com>
2023-06-05 17:28:47 -0300

    Seção de comentários
    Criando as funcionalidade para os comentários

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {UserContext} from '../../UserContext'
+import PhotoCommentsForm from "./PhotoCommentsForm";
+import styles from './PhotoComments.module.css'
 
-const PhotoComments = () => {
-    return <></>
+const PhotoComments = (props) => {
+
+    const [comments, setComments] = React.useState(() => props.comments)
+    const {login} = React.useContext(UserContext)
+
+    console.log(comments)
+
+    return (
+        <>
+            <ul className={styles.comments}>
+                {comments.map(comment => 
+                <li key={comment.comment_ID}>
+                    <b>{comment.comment_author}: </b>
+                    <span>{comment.comment_content}</span>
+                </li>)}
+            </ul>
+
+            {login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
+        </>
+    )
 }
 
 export default PhotoComments
